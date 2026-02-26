033507637b1d5affc4b047878f2d93a4bf845338
vickbk <vickbake8@gmail.com>
2026-02-26 13:14:57 +0200

    feat: add IconButtons component and integrate into MuiButton

--- src/app/components/mui-button.tsx
+++ src/app/components/mui-button.tsx
@@ -1,5 +1,6 @@
 import { ButtonColors } from "@/features/mui-buttons/components/button-colors";
 import { ButtonSize } from "@/features/mui-buttons/components/button-size";
+import { IconButtons } from "@/features/mui-buttons/components/icon-buttons";
 import { MuiButtonTypes } from "@/features/mui-buttons/components/mui-button-types";
 import { Stack, Typography } from "@mui/material";
 
@@ -12,6 +13,7 @@ export const MuiButton = () => {
       <MuiButtonTypes />
       <ButtonColors />
       <ButtonSize />
+      <IconButtons />
     </Stack>
   );
 };
